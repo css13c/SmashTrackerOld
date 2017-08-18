cb0cc2b200eb8c44de15fca00e433266b7fbc4b8
Connor Smith <aggiesuperfan@gmail.com>
2017-08-18 09:51:38 -0500

    WIP - Add UI

--- src/SmashTracker/App.xaml.cs
+++ src/SmashTracker/App.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ChallongeApiClient;
+using PlayerData;
+using System;
+using System.IO;
 using System.Windows;
 
 namespace SmashTracker
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
     {
-    }
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			m_playerDatabase = ApplicationDataUtility.LoadPlayerData(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+			m_challongeClient = new ChallongeClient();
+		}
+
+		protected override void OnExit(ExitEventArgs e)
+		{
+			base.OnExit(e);
+
+			ApplicationDataUtility.SavePlayerData(m_playerDatabase);
+		}
+
+		private PlayerDatabase m_playerDatabase;
+		private ChallongeClient m_challongeClient;
+	}
 }
