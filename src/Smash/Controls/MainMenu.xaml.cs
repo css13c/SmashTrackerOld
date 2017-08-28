a9f965f29676213a44a1da21b7887ec242c9b844
Connor Smith <aggiesuperfan@gmail.com>
2017-08-28 14:15:44 -0500

    Begin UI Work

---
+++ src/Smash/Controls/MainMenu.xaml.cs
@@ -0,0 +1,27 @@
+﻿using SmashTracker.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SmashTracker.Controls
+{
+	/// <summary>
+	/// Interaction logic for MainMenu.xaml
+	/// </summary>
+	public partial class MainMenu : UserControl
+	{
+		public MainMenu()
+		{
+			InitializeComponent();
+		}
+
+		private void Players_Click(object sender, RoutedEventArgs e)
+		{
+			PageSwitcher.Navigate(new PlayersPage());
+		}
+	}
+}
---
+++ src/Smash/Utilities/PageSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SmashTracker.Utilities
+{
+	public static class PageSwitcher
+	{
+		public static MainWindow Window;
+
+		public static void Navigate(UserControl newPage)
+		{
+			Window.Navigate(newPage);
+		}
+	}
+}
